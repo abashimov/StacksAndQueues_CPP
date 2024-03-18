c5d2211bde1f02e3fe47eb59b27381270ccf4a2f
Atabay Bashimov <migooface27@gmail.com>
2024-03-18 12:43:16 +0100

    Initial Commit

---
+++ StackandQueue/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  StackandQueue
+//
+//  Created by Atabay on 2024/3/18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
